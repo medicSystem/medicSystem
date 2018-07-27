198930ac18dac973c9cc0af4b325b45dafe062ca
ebortnik <mygodelphp@gmail.com>
2018-07-27 15:50:19 +0300

    Add jsone decode News in controllers/database

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,4 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/directory/{path?}', ['uses' => 'ReactController@index', 'as' => 'react', 'where' => ['path' => '.*']]);
 Route::get('/uploadImage', ['uses' => 'UploadImageController@upload', 'as' => 'uploadImage']);
 Route::get('/dictionary', 'Database\UploadDictionary@getDirectories')->name('dictionary');
+Route::get('/news', 'Database\UploadNews@getNews')->name('news');
