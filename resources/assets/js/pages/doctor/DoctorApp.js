32765e9497b90188f8454dac7650815658796efd
batashof <batashof@gmail.com>
2018-08-28 12:24:31 +0300

    doctor started

--- resources/assets/js/pages/doctor/DoctorApp.js
+++ resources/assets/js/pages/doctor/DoctorApp.js
@@ -1,30 +1,43 @@
-import React, { Component } from 'react';
-import '../../App.css';
-import { Link } from 'react-router-dom';
-
+import React, { Component } from "react";
+import "../../App.css";
+import { Link } from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
 
 class DoctorApp extends Component {
-    render() {
-        return (
-            <div className="mdl-layout mdl-js-layout mdl-layout--fixed-drawer">
-                <div className="mdl-layout__drawer">
-                    <span className="mdl-layout-title">AdminPanel</span>
-                    <nav className="mdl-navigation">
-                        <Link to='#' className="mdl-navigation__link">Users</Link>
-                        <Link to='#' className="mdl-navigation__link" href="">Doctors</Link>
-                        <Link to='#' className="mdl-navigation__link" href="">News</Link>
-                        <Link to='#' className="mdl-navigation__link" href="">Coupons</Link>
-                        <Link to='#' className="mdl-navigation__link" href="">Notifications</Link>
-                        <Link to='#' className="mdl-navigation__link" href="">Support</Link>
-                    </nav>
-                </div>
-                <main className="mdl-layout__content">
-                    <div className="page-content">
-                    </div>
-                </main>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="mdl-layout mdl-js-layout mdl-layout--fixed-drawer">
+        <div className="mdl-layout__drawer">
+          <LinkContainer to="#" className="mdl-navigation__link">
+            <span className="mdl-layout-title">Doctor</span>
+          </LinkContainer>
+          <nav className="mdl-navigation">
+            <Link to="#" className="mdl-navigation__link">
+              Users
+            </Link>
+            <Link to="#" className="mdl-navigation__link" href="">
+              Doctors
+            </Link>
+            <Link to="#" className="mdl-navigation__link" href="">
+              News
+            </Link>
+            <Link to="#" className="mdl-navigation__link" href="">
+              Coupons
+            </Link>
+            <Link to="#" className="mdl-navigation__link" href="">
+              Notifications
+            </Link>
+            <Link to="#" className="mdl-navigation__link" href="">
+              Support
+            </Link>
+          </nav>
+        </div>
+        <main className="mdl-layout__content">
+          <div className="page-content" />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default DoctorApp;
