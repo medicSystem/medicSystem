dcb5de307a7a6dc077c408eb884fadd3dd0d9c8f
dmoroz <mr.djem1998@mail.ru>
2018-08-28 17:03:35 +0300

    fix patients controller

--- app/Http/Controllers/Database/PatientsController.php
+++ app/Http/Controllers/Database/PatientsController.php
@@ -15,7 +15,11 @@ class PatientsController extends Controller
         $i = 0;
         foreach ($patients as $patient) {
             $user_id[$i] = $patient->users_id;
-            $patientsInfo[$i] = DB::select('SELECT `users`.`first_name`,`users`.`id`,`users`.`last_name`,`users`.`email`,`users`.`birthday`,`users`.`phone_number`,`users`.`avatar`,`users`.`role`,`patients`.`id` AS `patients_id` FROM `users` JOIN `patients` ON `users`.`id` = `patients`.`users_id` WHERE `patients`.`users_id` = ' . $user_id[$i]);
+            $list = DB::select('SELECT `users`.`first_name`,`users`.`id`,`users`.`last_name`,`users`.`email`,`users`.`birthday`,`users`.`phone_number`,`users`.`avatar`,`users`.`role`,`patients`.`id` AS `patients_id` FROM `users` JOIN `patients` ON `users`.`id` = `patients`.`users_id` WHERE `patients`.`users_id` = ' . $user_id[$i]);
+            foreach ($list as $item){
+                $patientsInfo[$i] = array("first_name"=>$item->first_name, "id" => $item->id, "last_name" => $item->last_name, "email" => $item->email, "birthday" => $item->birthday,
+                    "phone_number" => $item->phone_number, "avatar" => $item->avatar, "role" => $item->role, "patients_id" => $item->patients_id);
+            }
             $i++;
         }
         $encodeInfo = json_encode($patientsInfo);
